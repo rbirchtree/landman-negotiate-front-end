3c125d600a8a3dcdfde36f5939ad6dea7f21e77a
rbirchtree <rbirchtree@gmail.com>
2019-03-25 18:17:25 -0700

    added form component w/o redux-form

--- src/components/Consultation.js
+++ src/components/Consultation.js
@@ -1,53 +1,170 @@
 import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
 import { login } from '../actions';
 import Input from './Input';
 //make login register client
-import {required, nonEmpty } from '../validators';
 
-class Consultation extends React.Component{
-	onSubmit(values){
-		return this.props.dispatch(login(values.firstName));
-		//return this.props.dispatch(consultation(v))
+
+export default class Consultation extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {firstName: 'John',
+					  lastName: 'Smith',
+					  unit: '8', 
+					  streetAddress	: 'sdf',
+					  city: '',
+					  USstate: '',
+					  zipCode:'',
+					  phoneNumber: '',
+					  email: '',
+					  notes: ''
+			};
+
+/*		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);*/
+	}
+
+	handleChange(event){
+		this.setState({value: event.target.value});
+	}
+
+	setfirstName(firstName){
+		this.setState({
+			firstName
+		});
+	}
+
+	setLastName(lastName){
+		this.setState({
+			lastName
+		});
+	}
+
+	setUnitNumber(unit){
+		this.setState({
+			unit
+		});
+	}
+
+	setStreetAddress(streetAddress){
+		this.setState({
+			streetAddress
+		});
+	}
+
+	setUSState(USstate){
+		this.setState({
+			USstate
+		});
+	}
+
+	setCity(city){
+		this.setState({
+			city
+		});
+	}
+
+	setPhoneNumber(phoneNumber){
+		this.setState({
+			phoneNumber
+		});
+	}
+
+	setZipCode(zipCode){
+		this.setState({
+			zipCode
+		});
+	}
+
+	setEmail(email){
+		this.setState({
+			email
+		});
+	}
+
+	setNotes(event){
+		this.setState({
+			notes : event.target.value
+		});
+	}
+
+	handleSubmit(event){
+		event.preventDefault();
+		alert(this.state.firstName);		
 	}
 
 	render(){
-	let error;
-	if (this.props.error){
-		error = (
-			<div className="form-error" aria-live="polite">
-				{this.props.error}
-			</div>
-			)
-	}
-	const {handleSubmit,pristine,reset,submitting} = this.props;
-
+		console.log(this.state)
 	return (
-		<form  className='consultForm' onSubmit={handleSubmit(values =>
-			this.onSubmit(values)
-			)}>
+		<form  className='consultForm' onSubmit={(e) =>this.handleSubmit(e)}>
 		 <div className='row'>
-		 <div className='col-md-4'>
-		 </div>
-		 <div >
-		  <label>First Name</label>
-		  <div className='col-md-4'>
-		   <Field
-		    name='firstName'
-		    component={Input}
-		    type='text'
-		    placeholder='First Name'
-		    validate={[required,nonEmpty]}
+		  <div className='col-md-6'>
+		    	<Input label="Last Name" value={this.state.lastName}
+		    	onChange={value => this.setLastName(value)}
 		    />
 		  </div>
+		  <div className='col-md-6'>
+		    <Input label="First Name" value={this.state.firstName}
+		    	onChange={value => this.setFirstName(value)}
+		    />
+		   </div>
 		 </div>
+		 <div className='row'>
+		  <div className='col-md-6'>
+		    	<Input label="Street Address" value={this.state.streetAddress}
+		    	onChange={value => this.setStreetAddress(value)}
+		    />
+		  </div>
+		  <div className='col-md-6'>
+		    <Input label="Unit Number" value={this.state.UnitNumber}
+		    	onChange={value => this.setUnitNumber(value)}
+		    />
+		   </div>
 		 </div>
-		 <button disabled={this.props.pristine || this.props.submitting}>
-		 	Log in
-		 </button>
+		 <div className='row'>
+		  <div className='col-md-6'>
+		    	<Input label="City" value={this.state.city}
+		    	onChange={value => this.setCity(value)}
+		    />
+		  </div>
+		  <div className='col-md-6'>
+		    <Input label="State" value={this.state.USstate}
+		    	onChange={value => this.setUSState(value)}
+		    />
+		   </div>
+		 </div>
+		 <div className='row'>
+		  <div className='col-md-6'>
+		    	<Input label="Phone Number" value={this.state.phoneNumber}
+		    	onChange={value => this.setPhoneNumber(value)}
+		    />
+		  </div>
+		  <div className='col-md-6'>
+		    <Input label="Zip Code" value={this.state.zipCode}
+		    	onChange={value => this.setZipCode(value)}
+		    />
+		   </div>
+		 </div>
+		 <div className='row'>
+		  <div className='col-md-6'>
+		    	<Input label="E-MAIL" value={this.state.email}
+		    	onChange={value => this.setEmail(value)}
+		    />
+		  </div>
+		  <div className='col-md-6'>
+		  	<label>Notes</label>
+		  	<textarea
+		  		value={this.state.value}
+		    	onChange={value => this.setNotes(value)}
+		    	/>
+		   </div>
+		 </div>
+		 <div className='row'>
+		  <button >
+		 	Submit
+		  </button>
+		 </div>
 		</form>
 		)
 	}
 }
 
-export default reduxForm({form: 'consultation'})(Consultation);
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,39 +1,12 @@
 import React from 'react';
 
-export default class Input extends React.Component {
-    componentDidUpdate(prevProps) {
-        if (!prevProps.meta.active && this.props.meta.active) {
-            this.input.focus();
-        }
-    }
-
-    render() {
-        let error;
-        if (this.props.meta.touched && this.props.meta.error) {
-            error = <div className="form-error">{this.props.meta.error}</div>;
-        }
-
-        let warning;
-        if (this.props.meta.touched && this.props.meta.warning) {
-            warning = (
-                <div className="form-warning">{this.props.meta.warning}</div>
-            );
-        }
-
-        return (
-            <div className="form-input">
-                <label htmlFor={this.props.input.name}>
-                    {this.props.label}
-                    {error}
-                    {warning}
-                </label>
-                <input
-                    {...this.props.input}
-                    id={this.props.input.name}
-                    type={this.props.type}
-                    ref={input => (this.input = input)}
-                />
-            </div>
-        );
-    }
+export default function Input(props) {
+    return (
+        <div>
+            <label htmlFor={props.id}>{props.label}</label>
+            <input type="text" id={props.id} min={props.min} max={props.max}
+                onChange={e => props.onChange(e.target.value)} value={props.value} />
+        </div>
+    );
 }
+
